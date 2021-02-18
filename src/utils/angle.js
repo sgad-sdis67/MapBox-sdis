82a64259935a1705d158452e89e8e06f77a5751d
Luc RATELLI <UHA-l.ratelli@presta.naos-it.net>
2021-02-18 14:36:27 +0100

    version with angle function latitude

--- src/utils/angle.js
+++ src/utils/angle.js
@@ -10,7 +10,7 @@ import along from '@turf/along';
 import distance from '@turf/distance';
 
 class Angle {
-
+ 
   constructor() {}
 
   addXPixelsToLng (lng, x, lat) {
@@ -21,31 +21,26 @@ class Angle {
     return lgnToPut;
   }
 
-  addMarkerToMap(e, lng, lat) {
+  addMarkerToMap(state, e, lng, lat) {
     this.angleDiv = document.createElement('div');
     this.angleDiv.textContent = "test";
     this.marker = new mapboxgl.Marker(this.angleDiv) 
     .setLngLat([this.addXPixelsToLng(lng, 50, lat), lat])
     .addTo(this.state.map);
-    this.lastAngle = this.calcAngle(this.state, e);
-    console.log('last angle', this.lastAngle)
   }
 
   calcAngle(state, e) {
-    console.log(e, 'event in calc');
     let angle;
     if (state.lastVertex) {
-      console.log('1', this.lastAngle);
-      angle = Math.round(bearing(state.lastVertex, [e.lngLat.lng, e.lngLat.lat]));
+      console.log('1', this.lastAngle, state.line.coordinates, [e.lngLat.lng, e.lngLat.lat]);
+      angle =Math.round(bearing(state.line.coordinates[state.line.coordinates.length - 2], [e.lngLat.lng, e.lngLat.lat]));
     } else {
-      console.log('2', state, e);
       angle = Math.round(bearing(state.line.coordinates[0], state.line.coordinates[1]));
     }
     return angle;
   }
 
   moveOn(state, e) {
-    console.log('11', state)
     if (this.angleDiv) {
       this.angleDiv.textContent = this.calcAngle(state, e);
     } 
@@ -54,9 +49,10 @@ class Angle {
   createAngleDiv(state, event, lng, lat) {
     this.state = state;
     if (this.angleDiv) {
+      this.lastAngle = this.calcAngle(state, event);
       this.marker.setLngLat([this.addXPixelsToLng(lng, 15, lat), lat]);
     } else {
-     this.addMarkerToMap(event, lng, lat);
+     this.addMarkerToMap(state, event, lng, lat);
     }
   }
 }
