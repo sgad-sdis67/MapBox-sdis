232a9717d881513bd3bdd95a0e700aba7ce503e1
RATELLI Luc <Luc.RATELLI@SDIS67.LOCAL>
2021-02-22 13:18:47 +0100

    right angle

--- src/utils/angle.js
+++ src/utils/angle.js
@@ -38,16 +38,17 @@ class Angle {
   definePointsAndCalculeAngle(state, e) {
     let point1;
     let point2;
+    let oldPoint;
     if (state.line) {
       point1 = state.line.coordinates[state.line.coordinates.length - 2];
       point2 = [e.lngLat.lng, e.lngLat.lat];
+      oldPoint = state.line.coordinates[state.line.coordinates.length - 3];
     } else {
       point1 = state.polygon.coordinates[0][state.polygon.coordinates[0].length - 2];
       point2 = [e.lngLat.lng, e.lngLat.lat];
+      oldPoint = state.polygon.coordinates[0][state.polygon.coordinates[0].length - 3];
     }
-
-    let angle = this.calcAngle(point1, point2);
-    console.log(e, 'event', angle)
+    let angle = this.calcAngle(point1, point2, oldPoint);
     if (this.snapping != 0) {
       let angleToSnap;
       const modulo = angle % this.snapping;
@@ -56,24 +57,17 @@ class Angle {
       } else {
         angleToSnap = Math.trunc(angle / this.snapping) * this.snapping;
       }
-      console.log(angleToSnap, state);
       this.drawAngleSnapped(angleToSnap - angle, point1, point2, state);
     }
     return angle;
   }
 
   drawAngleSnapped(angle, point, point2, state) {
-    var bearing1 = bearing(point, point2);
-    var azimuth = bearingToAzimuth(bearing1);
-    var distanceCalculated = distance(point, point2); 
-    var hypotenuse = distanceCalculated / Math.cos(angle);
-    console.log("arrivée", point2)
     var obj = { 
       type: 'LineString',
       coordinates: [point, point2]
     }
     obj = transformRotate(obj, angle, {pivot: point})
-    console.log(point, point2, angle, Math.sin(angle), Math.cos(angle), azimuth, this.lastAngle)
     this.snapPoint = obj.coordinates[1];
     if (!state.map.getSource('snapLine')) {
       state.map.addSource('snapLine', { type: 'geojson', data: obj });
@@ -82,9 +76,9 @@ class Angle {
         'type': 'line',
         'source': 'snapLine',
         'paint': {
-        'line-color': 'yellow',
+        'line-color': 'red',
         'line-opacity': 0.75,
-        'line-width': 5
+        'line-width': 1
         }
         });
     } else {
@@ -93,16 +87,15 @@ class Angle {
     }
   }
 
-  calcAngle(point1, point2) {
+  calcAngle(point1, point2, oldPoint) {
     let angle;
     const absoluteAngle = Math.round(bearing(point1, point2));
-    if (absoluteAngle < 0) {
-      angle = 360 + absoluteAngle;
-    } else {
-      angle = absoluteAngle;
+    let oldBearing = 0;
+    if (oldPoint !== undefined) {
+      oldBearing = Math.round(bearing(point1, oldPoint));
     }
-    this.lastCalcul = (this.lastAngle + angle)%360;
-    return this.lastCalcul;
+    angle = Math.abs(oldBearing - absoluteAngle) % 180; 
+    return angle;
   }
 
   moveOn(state, e) {
@@ -113,6 +106,7 @@ class Angle {
 
   createAngleDiv(state, event, lng, lat) {
     this.state = state;
+    console.log(state, 'state')
     if (this.angleDiv) {
       this.lastAngle = (this.lastAngle + this.lastCalcul)%360;
       this.marker.setLngLat([this.addXPixelsToLng(lng, 15, lat), lat]);
