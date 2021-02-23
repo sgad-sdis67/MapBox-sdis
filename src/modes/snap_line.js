2cf81e6fff42cbfe394447fb8c0f899de57055a4
RATELLI Luc <Luc.RATELLI@SDIS67.LOCAL>
2021-02-23 14:28:45 +0100

    snap in actual points

--- src/modes/snap_line.js
+++ src/modes/snap_line.js
@@ -1,169 +1,173 @@
 import {
-  geojsonTypes,
-  modes,
-  cursors,
+    geojsonTypes,
+    modes,
+    cursors,
 } from "@mapbox/mapbox-gl-draw/src/constants";
 import doubleClickZoom from "@mapbox/mapbox-gl-draw/src/lib/double_click_zoom";
 import DrawLine from "@mapbox/mapbox-gl-draw/src/modes/draw_line_string";
 import {
-  addPointTovertices,
-  createSnapList,
-  getGuideFeature,
-  IDS,
-  shouldHideGuide,
-  addLineToSnapList,
-  snap
+    addPointTovertices,
+    createSnapList,
+    getGuideFeature,
+    IDS,
+    shouldHideGuide,
+    addLineToSnapList,
+    snap
 } from "./../utils";
-import Angle from './../utils/angle.js'
+import Angle from './../utils/angle.js';
+import { v4 as uuidv4 } from 'uuid';
 
-const SnapLineMode = { ...DrawLine };
+const SnapLineMode = {...DrawLine };
 
-SnapLineMode.onSetup = function (options) {
-  const line = this.newFeature({
-    type: geojsonTypes.FEATURE,
-    properties: {},
-    geometry: {
-      type: geojsonTypes.LINE_STRING,
-      coordinates: [[]],
-    },
-  });
+SnapLineMode.onSetup = function(options) {
+    const line = this.newFeature({
+        type: geojsonTypes.FEATURE,
+        properties: {},
+        geometry: {
+            type: geojsonTypes.LINE_STRING,
+            coordinates: [
+                []
+            ],
+        },
+    });
 
-  const verticalGuide = this.newFeature(getGuideFeature(IDS.VERTICAL_GUIDE));
-  const horizontalGuide = this.newFeature(
-    getGuideFeature(IDS.HORIZONTAL_GUIDE)
-  );
+    const verticalGuide = this.newFeature(getGuideFeature(IDS.VERTICAL_GUIDE));
+    const horizontalGuide = this.newFeature(
+        getGuideFeature(IDS.HORIZONTAL_GUIDE)
+    );
 
-  this.addFeature(line);
-  this.addFeature(verticalGuide);
-  this.addFeature(horizontalGuide);
+    this.addFeature(line);
+    this.addFeature(verticalGuide);
+    this.addFeature(horizontalGuide);
 
-  const selectedFeatures = this.getSelected();
-  this.clearSelectedFeatures();
-  doubleClickZoom.disable(this);
+    const selectedFeatures = this.getSelected();
+    this.clearSelectedFeatures();
+    doubleClickZoom.disable(this);
 
-  const [snapList, vertices] = createSnapList(this.map, this._ctx.api, line);
-
-  const state = {
-    map: this.map,
-    line,
-    currentVertexPosition: 0,
-    vertices,
-    snapList,
-    selectedFeatures,
-    verticalGuide,
-    horizontalGuide,
-    direction: "forward", // expected by DrawLineString
-  };
-
-  state.options = this._ctx.options;
-
-  const moveendCallback = () => {
     const [snapList, vertices] = createSnapList(this.map, this._ctx.api, line);
-    state.vertices = vertices;
-    state.snapList = snapList;
-  };
-  // for removing listener later on close
-  state["moveendCallback"] = moveendCallback;
 
-  const optionsChangedCallBAck = (options) => {
-    state.options = options;
-  };
-  // for removing listener later on close
-  state["optionsChangedCallBAck"] = optionsChangedCallBAck;
+    const state = {
+        map: this.map,
+        line,
+        currentVertexPosition: 0,
+        vertices,
+        snapList,
+        selectedFeatures,
+        verticalGuide,
+        horizontalGuide,
+        direction: "forward", // expected by DrawLineString
+    };
 
-  this.map.on("moveend", moveendCallback);
-  this.map.on("draw.snap.options_changed", optionsChangedCallBAck);
-  state.angle = new Angle();
+    state.options = this._ctx.options;
 
-  return state;
+    const moveendCallback = () => {
+        const [snapList, vertices] = createSnapList(this.map, this._ctx.api, line);
+        state.vertices = vertices;
+        state.snapList = snapList;
+    };
+    // for removing listener later on close
+    state["moveendCallback"] = moveendCallback;
+
+    const optionsChangedCallBAck = (options) => {
+        state.options = options;
+    };
+    // for removing listener later on close
+    state["optionsChangedCallBAck"] = optionsChangedCallBAck;
+
+    this.map.on("moveend", moveendCallback);
+    this.map.on("draw.snap.options_changed", optionsChangedCallBAck);
+    state.angle = new Angle();
+    state.id = uuidv4();
+
+    return state;
 };
 
-SnapLineMode.onClick = function (state, e) {
-  // We save some processing by rounding on click, not mousemove
-  var lng = state.snappedLng;
-  var lat = state.snappedLat;
-  /* if (state.angle.snapPoint) {
-    lng = state.angle.snapPoint[0];
-    lat = state.angle.snapPoint[1];
-  } */
-  // End the drawing if this click is on the previous position
-  // Note: not bothering with 'direction'
-  if (state.currentVertexPosition > 0) {
-    const lastVertex = state.line.coordinates[state.currentVertexPosition - 1];
+SnapLineMode.onClick = function(state, e) {
+    // We save some processing by rounding on click, not mousemove
+    var lng = state.snappedLng;
+    var lat = state.snappedLat;
+    /* if (state.angle.snapPoint) {
+      lng = state.angle.snapPoint[0];
+      lat = state.angle.snapPoint[1];
+    } */
+    // End the drawing if this click is on the previous position
+    // Note: not bothering with 'direction'
+    if (state.currentVertexPosition > 0) {
+        const lastVertex = state.line.coordinates[state.currentVertexPosition - 1];
 
-    state.lastVertex = lastVertex;
+        state.lastVertex = lastVertex;
 
-    if (lastVertex[0] === lng && lastVertex[1] === lat) {
-      return this.changeMode(modes.SIMPLE_SELECT, {
-        featureIds: [state.line.id],
-      });
+        if (lastVertex[0] === lng && lastVertex[1] === lat) {
+            return this.changeMode(modes.SIMPLE_SELECT, {
+                featureIds: [state.line.id],
+            });
+        }
     }
-  }
 
-  // const point = state.map.project({ lng: lng, lat: lat });
+    // const point = state.map.project({ lng: lng, lat: lat });
 
-  addPointTovertices(state.map, state.vertices, { lng, lat });
+    addPointTovertices(state.map, state.vertices, { lng, lat });
 
-  state.line.updateCoordinate(state.currentVertexPosition, lng, lat);
+    state.line.updateCoordinate(state.currentVertexPosition, lng, lat);
 
-  state.currentVertexPosition++;
+    state.currentVertexPosition++;
 
-  state.line.updateCoordinate(state.currentVertexPosition, lng, lat);
-  state.angle.createAngleDiv(state, e, lng, lat);
-  console.log(state.line);
-  if (state.line.length > 2) {
-    console.log('dedans', lng, lat)
-    addLineToSnapList([ lng, lat ], state);// ajouter un système pour ajouter les points au layer au lieu d'ajouter des layers
-  }
-  
-  console.log(state.snapList)
+    state.line.updateCoordinate(state.currentVertexPosition, lng, lat);
+    state.angle.createAngleDiv(state, e, lng, lat);
+    console.log(state.line, 'state line');
+    if (state.line.coordinates.length > 3) {
+        console.log('dedans', lng, lat, state.line);
+        addLineToSnapList(state.line.coordinates.slice(0, state.line.coordinates.length - 1), state); // ajouter un système pour ajouter les points au layer au lieu d'ajouter des layers
+    }
+
+    console.log(state.snapList)
 };
 
-SnapLineMode.onMouseMove = function (state, e) {
+SnapLineMode.onMouseMove = function(state, e) {
 
-  const { lng, lat } = snap(state, e);
-  state.line.updateCoordinate(state.currentVertexPosition, lng, lat);
-  state.snappedLng = lng;
-  state.snappedLat = lat;
+    const { lng, lat } = snap(state, e);
+    state.line.updateCoordinate(state.currentVertexPosition, lng, lat);
+    state.snappedLng = lng;
+    state.snappedLat = lat;
 
-  if (
-    state.lastVertex &&
-    state.lastVertex[0] === lng &&
-    state.lastVertex[1] === lat
-  ) {
-    this.updateUIClasses({ mouse: cursors.POINTER });
+    if (
+        state.lastVertex &&
+        state.lastVertex[0] === lng &&
+        state.lastVertex[1] === lat
+    ) {
+        this.updateUIClasses({ mouse: cursors.POINTER });
 
-    // cursor options:
-    // ADD: "add"
-    // DRAG: "drag"
-    // MOVE: "move"
-    // NONE: "none"
-    // POINTER: "pointer"
-  } else {
-    this.updateUIClasses({ mouse: cursors.ADD });
-  }
-  state.angle.moveOn(state, e);
+        // cursor options:
+        // ADD: "add"
+        // DRAG: "drag"
+        // MOVE: "move"
+        // NONE: "none"
+        // POINTER: "pointer"
+    } else {
+        this.updateUIClasses({ mouse: cursors.ADD });
+    }
+    state.angle.moveOn(state, e);
 };
 
 // This is 'extending' DrawLine.toDisplayFeatures
-SnapLineMode.toDisplayFeatures = function (state, geojson, display) {
-  if (shouldHideGuide(state, geojson)) return;
+SnapLineMode.toDisplayFeatures = function(state, geojson, display) {
+    if (shouldHideGuide(state, geojson)) return;
 
-  // This relies on the the state of SnapLineMode being similar to DrawLine
-  DrawLine.toDisplayFeatures(state, geojson, display);
+    // This relies on the the state of SnapLineMode being similar to DrawLine
+    DrawLine.toDisplayFeatures(state, geojson, display);
 };
 
 // This is 'extending' DrawLine.onStop
-SnapLineMode.onStop = function (state) {
-  this.deleteFeature(IDS.VERTICAL_GUIDE, { silent: true });
-  this.deleteFeature(IDS.HORIZONTAL_GUIDE, { silent: true });
+SnapLineMode.onStop = function(state) {
+    this.deleteFeature(IDS.VERTICAL_GUIDE, { silent: true });
+    this.deleteFeature(IDS.HORIZONTAL_GUIDE, { silent: true });
 
-  // remove moveemd callback
-  this.map.off("moveend", state.moveendCallback);
+    // remove moveemd callback
+    this.map.off("moveend", state.moveendCallback);
 
-  // This relies on the the state of SnapLineMode being similar to DrawLine
-  DrawLine.onStop.call(this, state);
-  state.angle.remove(state);
+    // This relies on the the state of SnapLineMode being similar to DrawLine
+    DrawLine.onStop.call(this, state);
+    state.angle.remove(state);
 };
 
 export default SnapLineMode;
--- src/utils/angle.js
+++ src/utils/angle.js
@@ -1,6 +1,6 @@
 import {
-  angleToMeters,
-  metersPerPixel 
+    angleToMeters,
+    metersPerPixel
 } from './index.js'
 
 import length from '@turf/length';
@@ -8,120 +8,120 @@ import bearing from '@turf/bearing';
 import destination from '@turf/destination';
 import along from '@turf/along';
 import distance from '@turf/distance';
-import { bearingToAzimuth, bearingToAngle,transformRotate } from '@turf/turf';
+import { bearingToAzimuth, bearingToAngle, transformRotate } from '@turf/turf';
 
 class Angle {
- 
-  constructor(withSnapping = 30) {
-    this.lastAngle = 0;
-    this.lastCalcul = 0;
-    this.snapping = withSnapping;
-  }
 
-
-  addXPixelsToLng (lng, x, lat) {
-    const lngInMeters = angleToMeters(lng);
-    let pixels = (lngInMeters/metersPerPixel(lat, this.state.map.getZoom())) + x;
-    const lngInMetersToPut = pixels * metersPerPixel(lat, this.state.map.getZoom());
-    const lgnToPut = ((lngInMetersToPut) * 0.00001)/1.132;
-    return lgnToPut;
-  }
-
-  addMarkerToMap(state, e, lng, lat) {
-    this.angleDiv = document.createElement('div');
-    this.angleDiv.textContent = "NaN";
-    this.marker = new mapboxgl.Marker(this.angleDiv) 
-    .setLngLat([this.addXPixelsToLng(lng, 50, lat), lat])
-    .addTo(this.state.map);
-  }
-
-  definePointsAndCalculeAngle(state, e) {
-    let point1;
-    let point2;
-    let oldPoint;
-    if (state.line) {
-      point1 = state.line.coordinates[state.line.coordinates.length - 2];
-      point2 = [e.lngLat.lng, e.lngLat.lat];
-      oldPoint = state.line.coordinates[state.line.coordinates.length - 3];
-    } else {
-      point1 = state.polygon.coordinates[0][state.polygon.coordinates[0].length - 2];
-      point2 = [e.lngLat.lng, e.lngLat.lat];
-      oldPoint = state.polygon.coordinates[0][state.polygon.coordinates[0].length - 3];
+    constructor(withSnapping = 30) {
+        this.lastAngle = 0;
+        this.lastCalcul = 0;
+        this.snapping = withSnapping;
     }
-    let angle = this.calcAngle(point1, point2, oldPoint);
-    if (this.snapping != 0) {
-      let angleToSnap;
-      const modulo = angle % this.snapping;
-      if (modulo > this.snapping /2) {
-        angleToSnap = Math.trunc(angle / this.snapping) * this.snapping + this.snapping;
-      } else {
-        angleToSnap = Math.trunc(angle / this.snapping) * this.snapping;
-      }
-      this.drawAngleSnapped(angleToSnap - angle, point1, point2, state);
-    }
-    return angle;
-  }
 
-  drawAngleSnapped(angle, point, point2, state) {
-    var obj = { 
-      type: 'LineString',
-      coordinates: [point, point2]
+
+    addXPixelsToLng(lng, x, lat) {
+        const lngInMeters = angleToMeters(lng);
+        let pixels = (lngInMeters / metersPerPixel(lat, this.state.map.getZoom())) + x;
+        const lngInMetersToPut = pixels * metersPerPixel(lat, this.state.map.getZoom());
+        const lgnToPut = ((lngInMetersToPut) * 0.00001) / 1.132;
+        return lgnToPut;
     }
-    obj = transformRotate(obj, angle, {pivot: point})
-    this.snapPoint = obj.coordinates[1];
-    if (!state.map.getSource('snapLine')) {
-      state.map.addSource('snapLine', { type: 'geojson', data: obj });
-      state.map.addLayer({
-        'id': 'snapLineLayer',
-        'type': 'line',
-        'source': 'snapLine',
-        'paint': {
-        'line-color': 'red',
-        'line-opacity': 0.75,
-        'line-width': 1
+
+    addMarkerToMap(state, e, lng, lat) {
+        this.angleDiv = document.createElement('div');
+        this.angleDiv.textContent = "NaN";
+        this.marker = new mapboxgl.Marker(this.angleDiv)
+            .setLngLat([this.addXPixelsToLng(lng, 50, lat), lat])
+            .addTo(this.state.map);
+    }
+
+    definePointsAndCalculeAngle(state, e) {
+        let point1;
+        let point2;
+        let oldPoint;
+        if (state.line) {
+            point1 = state.line.coordinates[state.line.coordinates.length - 2];
+            point2 = [e.lngLat.lng, e.lngLat.lat];
+            oldPoint = state.line.coordinates[state.line.coordinates.length - 3];
+        } else {
+            point1 = state.polygon.coordinates[0][state.polygon.coordinates[0].length - 2];
+            point2 = [e.lngLat.lng, e.lngLat.lat];
+            oldPoint = state.polygon.coordinates[0][state.polygon.coordinates[0].length - 3];
         }
-        });
-    } else {
-      state.map.setLayoutProperty('snapLineLayer', 'visibility', 'visible');
-      state.map.getSource('snapLine').setData(obj)
+        let angle = this.calcAngle(point1, point2, oldPoint);
+        if (this.snapping != 0) {
+            let angleToSnap;
+            const modulo = angle % this.snapping;
+            if (modulo > this.snapping / 2) {
+                angleToSnap = Math.trunc(angle / this.snapping) * this.snapping + this.snapping;
+            } else {
+                angleToSnap = Math.trunc(angle / this.snapping) * this.snapping;
+            }
+            this.drawAngleSnapped(angleToSnap - angle, point1, point2, state);
+        }
+        return angle;
     }
-  }
 
-  calcAngle(point1, point2, oldPoint) {
-    let angle;
-    const absoluteAngle = Math.round(bearing(point1, point2));
-    let oldBearing = 0;
-    if (oldPoint !== undefined) {
-      oldBearing = Math.round(bearing(point1, oldPoint));
+    drawAngleSnapped(angle, point, point2, state) {
+        var obj = {
+            type: 'LineString',
+            coordinates: [point, point2]
+        }
+        obj = transformRotate(obj, angle, { pivot: point })
+        this.snapPoint = obj.coordinates[1];
+        if (!state.map.getSource('snapLine')) {
+            state.map.addSource('snapLine', { type: 'geojson', data: obj });
+            state.map.addLayer({
+                'id': 'snapLineLayer',
+                'type': 'line',
+                'source': 'snapLine',
+                'paint': {
+                    'line-color': 'red',
+                    'line-opacity': 0.75,
+                    'line-width': 1
+                }
+            });
+        } else {
+            state.map.setLayoutProperty('snapLineLayer', 'visibility', 'visible');
+            state.map.getSource('snapLine').setData(obj)
+        }
     }
-    angle = Math.abs(oldBearing - absoluteAngle) % 180; 
-    return angle;
-  }
 
-  moveOn(state, e) {
-    if (this.angleDiv) {
-      this.angleDiv.textContent = this.definePointsAndCalculeAngle(state, e);
-    } 
-  }
+    calcAngle(point1, point2, oldPoint) {
+        let angle;
+        const absoluteAngle = Math.round(bearing(point1, point2));
+        let oldBearing = 0;
+        if (oldPoint !== undefined) {
+            oldBearing = Math.round(bearing(point1, oldPoint));
+        }
+        angle = Math.abs(oldBearing - absoluteAngle) % 180;
+        return angle;
+    }
 
-  createAngleDiv(state, event, lng, lat) {
-    this.state = state;
-    if (this.angleDiv) {
-      this.lastAngle = (this.lastAngle + this.lastCalcul)%360;
-      this.marker.setLngLat([this.addXPixelsToLng(lng, 15, lat), lat]);
-    } else {
-     this.addMarkerToMap(state, event, lng, lat);
+    moveOn(state, e) {
+        if (this.angleDiv) {
+            this.angleDiv.textContent = this.definePointsAndCalculeAngle(state, e);
+        }
     }
-  }
 
-  remove(state) {
-    if (this.marker) {
-      this.marker.remove();
+    createAngleDiv(state, event, lng, lat) {
+        this.state = state;
+        if (this.angleDiv) {
+            this.lastAngle = (this.lastAngle + this.lastCalcul) % 360;
+            this.marker.setLngLat([this.addXPixelsToLng(lng, 15, lat), lat]);
+        } else {
+            this.addMarkerToMap(state, event, lng, lat);
+        }
     }
-    if (state.map.getSource('snapLine')) {
-      state.map.setLayoutProperty('snapLineLayer', 'visibility', 'none');
+
+    remove(state) {
+        if (this.marker) {
+            this.marker.remove();
+        }
+        if (state.map.getSource('snapLine')) {
+            state.map.setLayoutProperty('snapLineLayer', 'visibility', 'none');
+        }
     }
-  }
 }
 
 export default Angle;
